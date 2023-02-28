7e3c9a3db03d999c50b73047d5f86f3ce5318f8d
Evans Rutto <robbyevans001@gmail.com>
2023-02-28 10:08:04 +0300

    created contstants.module.scss

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import './constants.module.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
