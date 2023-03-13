223014fd2c452aaeb68482fe1edeb0de37c26bf8
Evans Rutto <robbyevans001@gmail.com>
2023-03-13 10:16:28 +0300

    updated menu

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -2,9 +2,18 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import css from "./Menu.module.scss";
+import { TfiMenu } from 'react-icons/tfi';
+
+
 // import {Button} from 'flowbite-react'
 
 function Menu() {
+  const [isOpen, setIsOpen] = useState(false)
+
+  function handleClick(){
+    setIsOpen(!isOpen)
+  }
+  
   return (
     <div className={css.wrapper}>
       <span className={css.logo}>
@@ -19,7 +28,33 @@ function Menu() {
           Locations
         </NavLink>
         {/* <NavLink className={css.btn}  to="/Episodes">Episodes</NavLink> */}
+
+
+
+
+
       </div>
+
+
+        {/* //hamburger menu for small screens */}
+      {/* <div>
+      <TfiMenu className={css.menu} onClick={handleClick}/>
+         {isOpen?(
+          <div className={css.hamburger}>
+          <NavLink className={css.nav} to="/">
+            Characters
+          </NavLink>
+          <NavLink className={css.nav} to="/Locations">
+            Locations
+          </NavLink>
+        </div>
+         ):(null)}
+      </div> */}
+
+
+
+     
+      
     </div>
   );
 }
